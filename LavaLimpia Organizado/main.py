4525ae2a25d76690f2ce8488a4c5a3485daba44a
Marco Vargas <marcoandres.vargas@alumnos.ulagos.cl>
2024-12-10 08:38:21 -0300

    Update main.py

--- LavaLimpia Organizado/main.py
+++ LavaLimpia Organizado/main.py
@@ -2,7 +2,7 @@ from Interfaz.ventanasApp import *
 
 ventanaInicial=ventanaPrincipal()
 
-if __name__ == "__main__":
-    ventanaInicial.mostrarVentana()
+if __name__ == "__main__": #comprueba si el archivo de esta ejecutando directamente 
+    ventanaInicial.mostrarVentana() #Se muestra la ventana de la interfaz 
     #ventanaInicial.cambiarVentana(ventanaRegistro)
     
